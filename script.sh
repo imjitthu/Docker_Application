1edf08e451a573bad19945dd8dd9c07cf8f04831
jithendar <jithendardharmapuri@gmail.com>
2022-09-13 00:41:49 +0530

    updated configuration

--- script.sh
+++ script.sh
@@ -1,11 +1,25 @@
-#/bin/bash
+#!/bin/bash
+
+COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+DOCKER="/usr/bin/docker"
+
+chmod +x ssl_renew.sh
+
+sudo crontab -e 
+
+*/5 * * * * /home/sammy/wordpress/ssl_renew.sh >> /var/log/cron.log 2>&1
+tail -f /var/log/cron.log
+
+cd /home/sammy/wordpress/
+$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+$DOCKER system prune -af
 
 echo "installing Required"
 yum update -y
 yum install certbot -y
 yum install haproxy -y
 
-
+nano ssl_renew.sh
 
 
 
