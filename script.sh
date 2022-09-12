062e88102cc97bfdea0fe6e94b68920fa9600d94
jithendar <jithendardharmapuri@gmail.com>
2022-09-13 00:30:44 +0530

    updated configuration

--- script.sh
+++ script.sh
@@ -7,3 +7,12 @@ yum install haproxy -y
 
 
 
+
+
+certbot certonly --webroot --webroot-path=/var/www/html --email contact@techlurn.org --agree-tos --no-eff-email --staging -d techlurn.org -d www.techlurn.org
+
+cd /etc/letsencrypt/live/www.techlurn.org
+cat cert.pem privatekey.pem > all..pem
+mv all..pem cert_privatekey.pem
+
+vim /etc/haproxy/haproxy.cfg
