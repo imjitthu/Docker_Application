f231871893e2532d37f3eef98cce56b9812554be
jithendar <jithendardharmapuri@gmail.com>
2022-09-13 04:04:58 +0530

    updated configuration

--- script.sh
+++ script.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-COMPOSE="/usr/local/bin/docker-compose --no-ansi"
-DOCKER="/usr/bin/docker"
-
-chmod +x ssl_renew.sh
-
-sudo crontab -e 
-
-*/5 * * * * /home/sammy/wordpress/ssl_renew.sh >> /var/log/cron.log 2>&1
-tail -f /var/log/cron.log
-
-cd /home/sammy/wordpress/
-$COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
-$DOCKER system prune -af
-
 echo "installing Required"
 yum update -y
 yum install certbot -y
-yum install haproxy -y
 
-nano ssl_renew.sh
+# COMPOSE="/usr/local/bin/docker-compose --no-ansi"
+# DOCKER="/usr/bin/docker"
 
-curl -sSLo nginx-conf/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
+# chmod +x ssl_renew.sh
+
+# sudo crontab -e 
+
+# */5 * * * * /home/sammy/wordpress/ssl_renew.sh >> /var/log/cron.log 2>&1
+# tail -f /var/log/cron.log
+
+# cd /home/sammy/wordpress/
+# $COMPOSE run certbot renew --dry-run && $COMPOSE kill -s SIGHUP webserver
+# $DOCKER system prune -af
+
+# nano ssl_renew.sh
+
+# curl -sSLo nginx-conf/options-ssl-nginx.conf https://raw.githubusercontent.com/certbot/certbot/master/certbot-nginx/certbot_nginx/_internal/tls_configs/options-ssl-nginx.conf
 
 
 # [ root@techlurno /tmp/WordPress_SSL_Docker ]# certbot certonly --webroot -w /var/www/html/ --renew-by-default --email contact@techlurn.org --text --agree-tos  -d techlurn.org -d www.techlurn.org
@@ -41,7 +40,7 @@ curl -sSLo nginx-conf/options-ssl-nginx.conf https://raw.githubusercontent.com/c
 #  * Donating to EFF:                    https://eff.org/donate-le
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-crontab -e
-Add the following line:
+# crontab -e
+# Add the following line:
 
-* */12 * * * root /usr/bin/certbot renew >/dev/null 2>&1
+# * */12 * * * root /usr/bin/certbot renew >/dev/null 2>&1
